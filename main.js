c62e0c9115cc63b00cc279a6e89f396f36e17c94
Anton Stepanov <iamusualguy@users.noreply.github.com>
2017-11-24 09:20:09 +0300

    Lets go!

---
+++ main.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const Excel = require('exceljs');
+
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+var $ = require('jquery');
+
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+function createWindow() {
+
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 550,
+        height: 150,
+        frame: false,
+        resizable: false
+    });
+
+    mainWindow.webContents.openDevTools();
+
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+}
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
+})
+
+app.on('ready', createWindow);
+
+
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });  //закрытие окна и сворачивание в док если это OS X
