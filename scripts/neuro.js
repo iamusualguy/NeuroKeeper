d76b80b8d20a2288898e7454625bf2553d69612e
Anton Stepanov <iamusualguy@users.noreply.github.com>
2017-11-24 12:23:20 +0300

    weekday array and hours arrray

--- scripts/neuro.js
+++ scripts/neuro.js
@@ -5,6 +5,28 @@ function addHoursToDate(d, h) {
     return d;
 }
 
+//get weekday array 
+//param: Date()
+function WeekDayArray(d) {
+    let res = [0, 0, 0, 0, 0, 0, 0];
+    res[d.getDay() - 1] = 1;
+    return res;
+}
+
+//get hours array 
+//param: Date()
+function HoursArray(d) {
+    var data = [];
+    var length = 23; // user defined length
+    
+    for(var i = 0; i < length; i++) {
+        data.push(0);
+    }
+
+    data[d.getHours()] = 1;
+    return data;
+}
+
 function GenerateDictionary(text) {
     text = text.replace(/[^a-zA-Z ]/g, "").toLowerCase();
     var tArray = text.split(" ");
