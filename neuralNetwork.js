2456401a5ed6fda8132f13fb36c9efeed7957e51
Nikita <791069083856@yandex.ru>
2018-05-15 22:37:12 +0300

    just check it

--- neuralNetwork.js
+++ neuralNetwork.js
@@ -12,15 +12,13 @@ function createNN() {
 	neuro.prepareData().then(function (trainingSet) {
 		trainingSet = neuro.normilize(trainingSet);
 		//for (i = 0; i < 20; i++) {
-		net.train(trainingSet[0],
-			{
-				errorThresh: 0.005,  // error threshold to reach
-				iterations: 100,   // maximum training iterations
-				log: true,           // console.log() progress periodically
-				logPeriod: 10,       // number of iterations between logging
-				//learningRate: 0.3    // learning rate
-			}
-		);
+		net.train(trainingSet[0], {
+			errorThresh: 0.005, // error threshold to reach
+			iterations: 100, // maximum training iterations
+			log: true, // console.log() progress periodically
+			logPeriod: 10, // number of iterations between logging
+			//learningRate: 0.3    // learning rate
+		});
 
 		let wstream = fs.createWriteStream('brain.json');
 		wstream.write(JSON.stringify(net.toJSON(), null, 2));
@@ -43,15 +41,13 @@ function createLSTN() {
 
 	neuro.prepareData().then(function (trainingSet) {
 		trainingSet = neuro.normilize(trainingSet);
-		net.train(trainingSet[0],
-			{
-				errorThresh: 0.005,  // error threshold to reach
-				iterations: 10000,   // maximum training iterations
-				log: true,           // console.log() progress periodically
-				logPeriod: 100,       // number of iterations between logging
-				//learningRate: 0.3    // learning rate
-			}
-		);
+		net.train(trainingSet[0], {
+			errorThresh: 0.005, // error threshold to reach
+			iterations: 10000, // maximum training iterations
+			log: true, // console.log() progress periodically
+			logPeriod: 100, // number of iterations between logging
+			//learningRate: 0.3    // learning rate
+		});
 
 		let wstream = fs.createWriteStream('brain.json');
 		wstream.write(JSON.stringify(net.toJSON(), null, 2));
@@ -63,7 +59,7 @@ function createLSTN() {
 }
 
 function openNN(i) {
-	var net = new brain.NeuralNetwork();//{
+	var net = new brain.NeuralNetwork(); //{
 	//activation: 'relu', // activation function
 	//hiddenLayers: [120, 60], // hiddenLayers: [3, 4]
 	//learningRate: 0.6 // общая степень обученности, полезна при обучении в несколько потоков
@@ -74,22 +70,22 @@ function openNN(i) {
 
 function getNextString() {
 	return neuro.gI().then((inputData) => {
-			console.log(inputData);
-			inputData = neuro.normilize(inputData);
-			var net = openNN(getRandomInt(1, 3));
-			//for (i = 0; i > trainingSet[0].lenght; i++) {
-			var output = net.run(inputData[0][0].input);
-			var res = neuro.toWords(output, inputData[1][0].input);
-			//var resNeed = neuro.toWords(inputData[0][i].output, inputData[1][i].output)
-			console.log(res);
-			//console.log(resNeed);
-			//	}
-			res = res.filter((v, i, a) => a.indexOf(v) === i)
+		console.log(inputData);
+		inputData = neuro.normilize(inputData);
+		var net = openNN(getRandomInt(1, 3));
+		//for (i = 0; i > trainingSet[0].lenght; i++) {
+		var output = net.run(inputData[0][0].input);
+		var res = neuro.toWords(output, inputData[1][0].input);
+		//var resNeed = neuro.toWords(inputData[0][i].output, inputData[1][i].output)
+		console.log(res);
+		//console.log(resNeed);
+		//	}
+		res = res.filter((v, i, a) => a.indexOf(v) === i)
 
-			res = res.slice(getRandomInt(1, 8),getRandomInt(13, 25));
-			console.log(res);
-			return res.join(" ");
-		});
+		res = res.slice(getRandomInt(1, 8), getRandomInt(13, 25));
+		console.log(res);
+		return res.join(" ");
+	});
 }
 
 function getRandomInt(min, max) {
